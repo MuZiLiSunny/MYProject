1d6aaba282a53bff61786cf6346e9248abea2ba3
LIJINSEN\HelloMrLi <lijinsen@wondersgroup.com>
2019-05-10 00:10:26 +0800

    Initial commit

---
+++ platform-common/src/main/java/com/wondersgroup/ylcloud/common/dao/EmployeeMapper.java
@@ -0,0 +1,18 @@
+package com.wondersgroup.ylcloud.common.dao;
+
+
+import com.wondersgroup.ylcloud.common.entity.Employee;
+
+public interface EmployeeMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Employee record);
+
+    int insertSelective(Employee record);
+
+    Employee selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Employee record);
+
+    int updateByPrimaryKey(Employee record);
+}
